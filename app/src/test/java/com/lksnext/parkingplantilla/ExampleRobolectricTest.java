d4134315946f7e5e3a0b85d35a956735f63de16c
aimarsanm <aimarsanm@gmail.com>
2025-07-09 18:32:40 +0200

    jacoco funciona?

---
+++ app/src/test/java/com/lksnext/parkingplantilla/ExampleRobolectricTest.java
@@ -0,0 +1,16 @@
+package com.lksnext.parkingplantilla;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class ExampleRobolectricTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
+
